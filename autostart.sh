021b5ad6f1b23460f05efb7d9284350bfff3588a
adam russell <adru@odoo.com>
2024-04-17 11:13:51 -0400

    init

---
+++ autostart.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+config=$(autorandr -c)
+autorandr -l $config &
+
+# Display different wallpapers for each monitor
+cmd='xwallpaper '
+if xrandr --listactivemonitors
+then
+    # xrandr is available: get output names from xrandr
+    for output in $(xrandr --listactivemonitors | cut -d ' ' -f6 )
+    do
+       cmd="$cmd --output $output --stretch /home/adam/.dwm/hashtags2.png"
+    done
+else
+    # fallback: xrandr not available
+    cmd="$cmd --no-randr --stretch  /home/adam/.dwm/hashtags2.png"
+fi
+$cmd
+
+
